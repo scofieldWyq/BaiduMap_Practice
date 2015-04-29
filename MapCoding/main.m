7be65432fd077fcf028cecb63d7660cd64eb80bf
scofieldWyq <147036scofield@gmail.com>
2015-04-29 16:18:40 +0800

    Initial Commit

---
+++ MapCoding/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapCoding
+//
+//  Created by 吴英强 on 15/4/29.
+//  Copyright (c) 2015年 吴英强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WYQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WYQAppDelegate class]));
+    }
+}
